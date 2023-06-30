f4acb78a49d68112d049555466ace670f868ecd8
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-30 19:27:53 +0100

    B2

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,46 +2,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 
 /**
- * infinite_while - Run efadsfasdf.
- *
- * Return: Aladad0.
- */
+* infinite_while - runs an infinit loop
+*
+* Return: it has a return but it will never reach it
+*/
 int infinite_while(void)
 {
-	while (1)
-	{
-		sleep(1);
-	}
-	return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 
 
 /**
- * main - Creatfdeaqdfeade.
- *
- * Return: Alwacfda0.
- */
+* main - the main method
+*
+* Return: 1 in success/ 0 in failure
+*/
 int main(void)
 {
-	pid_t pid;
-	char count = 0;
+int zombies_to_make = 5;
 
-	while (count < 5)
-	{
-		pid = fork();
-		if (pid > 0)
-		{
-			printf("Zombie process created, PID: %d\n", pid);
-			sleep(1);
-			count++;
-		}
-		else
-			exit(0);
-	}
+for (int i = 0; i < zombies_to_make; i++)
+{
+pid_t chld_PID;
 
-	infinite_while();
-	return (EXIT_SUCCESS);
+chld_PID = fork();
+
+if (chld_PID > 0)
+{
+printf("Zombie process created, PID: %d\n", chld_PID);
+}
+else if (chld_PID == 0)
+{
+return (0);
+}
+else
+{
+printf("fork failed");
+return (-1);
+}
+}
+
+infinite_while();
+return (0);
 }
