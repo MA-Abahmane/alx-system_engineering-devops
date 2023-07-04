c8de52dce37112c085ad91068d24fdee0a4873d8
Mohamed Amine <ma.abahmane@gmail.com>
2023-07-04 14:39:23 +0100

    rb2

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-txt_from = ARGV[0].scan(/from:([^]]+)/).flatten
-txt_to = ARGV[0].scan(/to:([^]]+)/).flatten
-txt_flags = ARGV[0].scan(/flags:([^]]+)/).flatten
+txt_from = ARGV[0].scan(/from:([^[\]]+)/).flatten
+txt_to = ARGV[0].scan(/to:([^[\]]+)/).flatten
+txt_flags = ARGV[0].scan(/flags:([^[\]]+)/).flatten
 
-rez = [txt_from, txt_to, txt_flags].joint(',')
+rez = [txt_from, txt_to, txt_flags].join(',')
 puts rez
