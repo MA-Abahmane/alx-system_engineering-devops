2e259308b84ac2632c43dbfcb96ac3405089fcd6
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-25 18:00:19 +0100

    Beholde, The Puppet master.

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
-# Using Puppet, create a manifest that kills a process named killmenow.
+# Using Puppet, create a manifest that kills a process named killmenow
 # Requirements:
 #   Must use the exec Puppet resource
 #   Must use pkill
