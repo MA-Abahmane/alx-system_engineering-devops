5175c2e98e79a0df49d98611c17d497c1f00a1e0
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-08-29 16:32:45 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,4 @@
+# #!/usr/bin/env bash
 # Time to practice configuring your server with Puppet! Just as you did before,
 # we’d like you to install and configure an Nginx server using Puppet instead of
 # Bash. To save time and effort, you should also include resources in your
@@ -11,23 +12,26 @@
 #   configure an Ubuntu machine to respect above requirements    
 
 
-class { 'nginx':
+# Make sure Nginx is install
+package { 'nginx':
   ensure => installed,
 }
 
-nginx::resource::server { 'default':
-  listen_port => 80,
-  locations   => {
-    '/' => {
-      location_cfg => [
-        'return 200 "Hello World!";',
-      ],
-    },
-    '/redirect_me' => {
-      location_cfg => [
-        'return 301 https://www.github.com/MA-Abahmane;',
-      ],
-    },
-  },
+# The redirection is Moved Permanently to my github and Nginx is listening on port 80
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/MA-Abahmane permanent;',
 }
 
+# Create web index page containing the string 'Hello World!'
+  file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Rerun nginx service
+  service { 'nginx': 
+  ensure  => running,
+  require => Package['nginx'],
+}
