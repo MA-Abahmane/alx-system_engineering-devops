bcfa485c5fe8e07b65f4dbd07defe793cd65dbf8
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-04 22:57:19 +0100

    w

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,8 +11,8 @@ command  => '
 sudo apt-get update;
 sudo apt-get install -y nginx;
 
-sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
 
-sudo service nginx restart',
+sudo service nginx restart;',
 provider => shell,
 }
