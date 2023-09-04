dc2f3d32cb2ab5ed6cec057184915f81088265d2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-04 22:53:48 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,15 +4,14 @@
 #   - The value of the custom HTTP header must be the hostname of the server Nginx is running on
 
 # Make sure nginx is Installed
-exec { 'install nginx':
-  command  => 'sudo apt-get update -y;
-	sudo apt-get install nginx -y;',
-  provider => shell,
-}
-
-# creating a custom HTTP header response
+# create a custom HTTP header response
+# Reload nginx to load changes
 exec { 'custom http header':
-  command  => 'sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-	sudo service nginx restart',
-  provider => shell,
+command  => 'sudo apt-get update;
+sudo apt-get install -y nginx;
+
+sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+
+sudo service nginx restart',
+provider => shell,
 }
