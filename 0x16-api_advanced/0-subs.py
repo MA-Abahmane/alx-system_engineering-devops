926d66516b13271154e6cf6cdb6f913dd03fc0a1
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 14:32:53 +0100

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and returns the number
-of subscribers for a given
-subreddit."""
+"""
+    Write a function that queries the Reddit API and returns the number
+    of subscribers for a given
+    subreddit.
+"""
 
 
 def number_of_subscribers(subreddit):
