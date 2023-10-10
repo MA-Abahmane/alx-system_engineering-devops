cf7b8972e3e25435233eee72d37fe13b63bfe4ee
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 14:35:23 +0100

    old_code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
     Write a function that queries the Reddit API and returns the number
-    of subscribers for a given
-    subreddit.
+     of subscribers (not active users, total subscribers) for a given
+     subreddit. If an invalid subreddit is given, the function should return 0.
+    import the requests library, which allows us to make HTTP requests to the
+     Reddit API.
 """
+
+import requests as req
 
 
 def number_of_subscribers(subreddit):
@@ -11,19 +15,29 @@ def number_of_subscribers(subreddit):
         A function that queries the Reddit API and returns the number of
         subscribers
     """
-    import requests
 
-
+    # By appending "/about.json" to the subreddit's URL, we can access various
+    #  details about the subreddit, including its number of subscribers,
+    #  description and more, in a structured JSON format.
     URL = f'https://www.reddit.com/r/{subreddit}/about.json'
 
+    # Define a custom user agent to avoid too many requests error
     headers = {
         'User-Agent': 'Reddit-Agent'
     }
 
-    response = requests.get(URL, headers=headers, allow_redirects=False)
+    # sending a GET request to reddits API
+    response = req.get(URL, headers=headers, allow_redirects=False)
 
-    if (response.status_code >= 300):
+    # successful request
+    if (response.status_code == 200):
+        # parse the response to a json string
+        Jstring = response.json()
+
+        # extract  the number of subscribers / return
+        subs_count = Jstring['data']['subscribers']
+
+        return subs_count
+    # in case of error
+    else:
         return 0
-
-    subs_count = response.json().get("data").get("subscribers")
-    return subs_count
