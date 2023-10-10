533e0d94dae586812c4d8e63f3297a30ef523787
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 14:51:38 +0100

    test2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,26 +13,17 @@ def top_ten(subreddit):
         first 10 hot posts
     """
 
-    # By appending "/about.json" to the subreddit's URL, we can access various
-    #  details about the subreddit, including its number of subscribers,
-    #  description and more, in a structured JSON format.
     URL = f'https://www.reddit.com/r/{subreddit}/hot.json'
 
-    # Define a custom user agent to avoid too many requests error
     headers = {
         'User-Agent': 'Reddit'
     }
 
-    # sending a GET request to reddits API
     response = req.get(URL, headers=headers, allow_redirects=False)
 
-    # successful request
     if (response.status_code == 200):
-        # parse the response to a json string
         Jstring = response.json()
 
-        # extract  the chrldrent from data if exists
-        # then extract the top 10 titles from children data
         if 'data' in Jstring and 'children' in Jstring['data']:
             posts = Jstring['data']['children']
             for i, post in enumerate(posts):
@@ -41,6 +32,5 @@ def top_ten(subreddit):
                     print(title)
         else:
             print('None')
-    # in case of error
     else:
         print('None')
