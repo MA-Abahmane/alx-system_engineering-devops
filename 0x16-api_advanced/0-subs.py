9a25557a96b380b510caaede50453d9781bcac0e
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 13:56:20 +0100

    test7

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
-"""
-    Write a function that queries the Reddit API and returns the number
-     of subscribers (not active users, total subscribers) for a given
-     subreddit. If an invalid subreddit is given, the function should return 0.
-    import the requests library, which allows us to make HTTP requests to the
-     Reddit API.
-"""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-        A function that queries the Reddit API and returns the number of
-        subscribers
-    """
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
 
-    if subreddit is None or type(subreddit) is not str:
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-
-    headers = {
-        'User-Agent': 'Reddit-Agent'
-    }
-
-    response = requests.get(URL, headers=headers, allow_redirects=False)
-
-
-    if (response.status_code >= 300):
-        return 0
-
-    subs_count = response.json().get("data").get("subscribers")
-
-    return subs_count
+    return sub_info.json().get("data").get("subscribers")
