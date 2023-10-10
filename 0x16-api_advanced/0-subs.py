199d1a90e104e1ab83666bcef738764e37b2f451
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 13:59:56 +0100

    test9

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-    Write a function that queries the Reddit API and returns the number
-     of subscribers (not active users, total subscribers) for a given
-     subreddit. If an invalid subreddit is given, the function should return 0.
-    import the requests library, which allows us to make HTTP requests to the
-     Reddit API.
-"""
+"""Write a function that queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers) for a given
+subreddit. If an invalid subreddit is given, the function should return 0.
+import the requests library, which allows us to make HTTP requests to the
+Reddit API."""
 
 
 def number_of_subscribers(subreddit):
-    """
-        A function that queries the Reddit API and returns the number of
-        subscribers
-    """
+    """A function that queries the Reddit API and returns the number of
+        subscribers"""
 
     import requests
 
