43c7047ab7df753b1ed9e60ac41c5d5d1745bfa1
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 23:15:31 +0100

    Finally 😤 Got it

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-    Write a recursive function that queries the Reddit API, parses the title of
-    all hot articles, and prints a sorted count of given keywords (case-insensitive,
-    delimited by spaces. Javascript should count as javascript, but java should not)
+    Write a recursive function that queries the Reddit API, parses the title
+    of all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count
+    as javascript, but java should not)
 """
 
-import requests
+import requests as req
 
 
 def count_words(subreddit, word_list, after=None, results=None):
@@ -14,38 +15,48 @@ def count_words(subreddit, word_list, after=None, results=None):
         all hot articles, and prints a sorted count of given keywords
     """
 
+    """ create dict to save results """
     if results is None:
         results = {}
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    URL = f'https://www.reddit.com/r/{subreddit}/hot.json'
 
     headers = {
-        "User-Agent": "My-User-Agent"
+        'User-Agent': 'Something'
     }
     params = {
-        "after": after
+        'after': after
     }
 
-    response = requests.get(url, params=params, headers=headers, allow_redirects=False)
+    """ Send GET request to URL and save the response """
+    response = req.get(URL, headers=headers, params=params,
+                       allow_redirects=False)
 
-    if response.status_code != 200:
+    if (response.status_code != 200):
         return
 
-    data = response.json().get("data")
-    hot_titles = [post["data"]["title"] for post in data.get("children")]
+    """ after parsing the response, save all hot titles is a list """
+    data = response.json()['data']
+    titles = [post['data']['title'] for post in data['children']]
 
-    for title in hot_titles:
-        words = title.lower().split()
+    """ lowercase and split title words to a list """
+    for title in titles:
+        Twords = title.lower().split()
+        """ compare each user word with the title list and count """
         for word in word_list:
-            word_count = words.count(word.lower())
-            if word_count > 0:
+            word_count = Twords.count(word)
+            if (word_count > 0):
                 results[word] = results.get(word, 0) + word_count
 
-    if not data.get("after"):
-        # Sort the results as specified
-        sorted_results = sorted(results.items(), key=lambda kv: (-kv[1], kv[0].lower()))
-        for word, count in sorted_results:
-            if count > 0:
-                print(f"{word.lower()}: {count}")
+    """ If there is no after page, return values """
+    if not data['after']:
+        """ Sort the results """
+        sorted_lst = sorted(results.items(), key=lambda kv:
+                            (-kv[1], kv[0].lower()))
+        """ print out resultes """
+        for word, count in sorted_lst:
+            if (count > 0):
+                print(f'{word.lower()}: {count}')
     else:
-        return count_words(subreddit, word_list, after=data.get("after"), results=results)
+        return count_words(subreddit, word_list, after=data['after'],
+                           results=results)
