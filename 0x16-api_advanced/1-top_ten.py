fb837ec0c2665779bdecab4ddd0799f369f4fcc3
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 19:49:13 +0100

    test3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-    Write a function that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit
-"""
+"""Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit"""
 
 import requests as req
 
 
 def top_ten(subreddit):
-    """
-        A function that queries the Reddit API and prints the titles of the
-        first 10 hot posts
-    """
+    """A function that queries the Reddit API and prints the titles of the
+        first 10 hot posts"""
 
     URL = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
 
