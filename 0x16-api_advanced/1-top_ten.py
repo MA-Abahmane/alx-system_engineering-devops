31eb529e2292431e9ee7b39d91284d0d95ad64f9
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 20:00:28 +0100

    test 6

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit"""
+"""
+    Write a function that queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit
+"""
 
 import requests as req
 
 
 def top_ten(subreddit):
-    """A function that queries the Reddit API and prints the titles of the
-        first 10 hot posts"""
+    """
+    A function that queries the Reddit API and prints the titles of the
+    first 10 hot posts
+    """
 
-    URL = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
     headers = {
         'User-Agent': 'Reddit-Agent'
     }
 
-    response = req.get(URL, headers=headers)
+    response = req.get(URL, headers=headers, allow_redirects=False)
 
-    if (response.status_code == 200):
-        Jstring = response.json()
-
-        posts = Jstring['data']['children']
-        for i, post in enumerate(posts):
-            if i <= 9:
-                title = post['data']['title']
-                print(title)
-    else:
+    if (response.status_code >= 300):
         print(None)
+    else:
+        Js = response.json()
+        [print(child.get("data").get("title")) for child in Js.get("data").get("children")]
