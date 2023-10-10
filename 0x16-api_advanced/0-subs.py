584c276f582a59f9774ed4e59420fad98fbe4b6d
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 13:13:52 +0100

    test 5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,24 +16,19 @@ def number_of_subscribers(subreddit):
         subscribers
     """
 
-    # By appending "/about.json" to the subreddit's URL, we can access various
-    #  details about the subreddit, including its number of subscribers,
-    #  description and more, in a structured JSON format.
     URL = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-    # Define a custom user agent to avoid too many requests error
+
     headers = {
         'User-Agent': 'Reddit-Agent'
     }
 
-    # sending a GET request to reddits API
     response = requests.get(URL, headers=headers, allow_redirects=False)
 
-    # error handling
+
     if (response.status_code >= 300):
         return 0
 
-    # parse the response to a json string
     subs_count = response.json().get("data").get("subscribers")
 
     return subs_count
