730eccda8f029f745991f0c587ce015c5e5e22e6
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-10 22:37:06 +0100

    push 100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    Write a recursive function that queries the Reddit API, parses the title of
+    all hot articles, and prints a sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as javascript, but java should not)
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after=None, word_count=None):
+    """
+        A recursive function that queries the Reddit API, parses the title of
+        all hot articles, and prints a sorted count of given keywords
+    """
+
+    if word_count is None:
+        word_count = {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    headers = {
+        "User-Agent": "My-User-Agent"
+    }
+    params = {
+        "after": after
+    }
+
+    response = requests.get(url, params=params, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get("data")
+    hot_titles = [post["data"]["title"] for post in data.get("children")]
+
+    for title in hot_titles:
+        words = title.lower().split()
+        for word in word_list:
+            word_count[word] = word_count.get(word, 0) + words.count(word.lower())
+
+    if not data.get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: (-kv[1], kv[0].lower()))
+        for word, count in sorted_counts:
+            if count > 0:
+                print(f"{word.lower()}: {count}")
+    else:
+        return count_words(subreddit, word_list, after=data.get("after"), word_count=word_count)
