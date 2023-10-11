0e01e857b73402e0af10dd1d2a4d8312a932f207
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-11 21:51:03 +0100

    Bug fixed 😎

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # I found the bug! In the wordpress file there was a file called 'wp-settings.php' with the extension 'phpp'.
 
 exec {'Fix File Extension':
-    command => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php'
+    command => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+    path    => '/bin:/usr/bin'
 }
