180b9183954bd2dd420144d4c16571044cf5ce0c
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-18 20:05:31 +0100

    Fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,7 @@
-# Change the OS configuration so that it is possible to login with the holberton user
-# and open a file without any error message.
+# Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 
 # Removes all hard and soft limits from the holberton user
-exec { 'Remove_limits':
+exec { 'RemoveLimitsDel':
     command  => "sed -i "/^holberton/d" /etc/security/limits.conf",
     provider => shell,
 }
