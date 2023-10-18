f937887696ba7c66ceff37a88caca37b9f421a84
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-18 20:02:08 +0100

    Limitless holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# Change the OS configuration so that it is possible to login with the holberton user
+# and open a file without any error message.
+
+# Removes all hard and soft limits from the holberton user
+exec { 'Remove_limits':
+    command  => "sed -i "/^holberton/d" /etc/security/limits.conf",
+    provider => shell,
+}
